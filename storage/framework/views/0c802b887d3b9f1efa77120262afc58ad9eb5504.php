f5515e6204650a4bd6da5d3e891324c0f514a591
Banchy77 <89152862+Banchy77@users.noreply.github.com>
2021-09-10 11:44:44 -0500

    Add files via upload

---
+++ storage/framework/views/0c802b887d3b9f1efa77120262afc58ad9eb5504.php
@@ -0,0 +1,10 @@
+<?php $__env->startSection('content'); ?>
+
+<?php echo $__env->make('dashboard.partials.validation-error', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<form action="<?php echo e(route("user.store")); ?>" method="POST">
+    <?php echo $__env->make('dashboard.user._form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</form>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('dashboard.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\larablog\resources\views/dashboard/user/create.blade.php ENDPATH**/ ?>
